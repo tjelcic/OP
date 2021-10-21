54d773a70b72408fbf0514cbefc1419c5ea030ee
tjelcic <38245828+tjelcic@users.noreply.github.com>
2021-10-21 20:45:57 +0200

    Update main.cpp

--- Vježba 2/Vj2Zad3/main.cpp
+++ Vježba 2/Vj2Zad3/main.cpp
@@ -20,15 +20,15 @@ typedef struct
 
 int findOverlap(const Rectangle (&arr)[4], const Circle &c, int n)
 {
-    Point Delta;
+    Point delta;
     int cnt=0;
 
     for(int i=0;i<n;i++)
     {
-        Delta.x=c.S.x - ((c.S.x < arr[i].a.x) ? arr[i].a.x : (c.S.x > arr[i].b.x) ? arr[i].b.x : c.S.x);
-        Delta.y=c.S.y - ((c.S.y < arr[i].a.y) ? arr[i].a.y : (c.S.x > arr[i].b.y) ? arr[i].b.y : c.S.y);
+        delta.x=c.S.x - ((c.S.x < arr[i].a.x) ? arr[i].a.x : (c.S.x > arr[i].b.x) ? arr[i].b.x : c.S.x);
+        delta.y=c.S.y - ((c.S.y < arr[i].a.y) ? arr[i].a.y : (c.S.x > arr[i].b.y) ? arr[i].b.y : c.S.y);
 
-        if((Delta.x * Delta.x + Delta.y * Delta.y) <= c.r * c.r)
+        if((delta.x * delta.x + delta.y * delta.y) <= c.r * c.r)
         {
             cnt++;
         }
@@ -41,6 +41,6 @@ int main()
     Rectangle arr[]={{{-1,-1},{-2,-2}},{{3,3},{4,4}},{{3,5},{5,8}},{{7,1},{8,3}}};
     Circle c={3,{3,3}};
     int n=sizeof (arr)/sizeof(arr[0]);
-    cout<<findOverlap(arr,c,n)<<" rectangles overlap with the circle"<<endl;
+    cout << findOverlap(arr,c,n) << " rectangles overlap with the circle" << endl;
     return 0;
 }
