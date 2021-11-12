73fc0611a85e13ad8728a4a9d13bdfb2482e3236
Tea Jelčić <38245828+tjelcic@users.noreply.github.com>
2021-11-12 08:55:03 +0100

    Add files via upload

--- Vježba 1/Vj1Zad1/main.cpp
+++ Vježba 1/Vj1Zad1/main.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 int main()
 {
-bool flag;
-cout << "unesi " << true << " - " << false << endl;
-cin >> flag;
-cout << noboolalpha << flag <<endl;
-int a = 123;
-cout << "hex " << hex << a << endl;
-cout << "dec " << dec << a << endl;
-cout << "oct " << oct << a << endl;
-double pi = 3.141592;
-cout << "pi = " << fixed;
-cout << setprecision(7) << setw(20) << setfill('*');
-cout << pi << endl;
+    bool flag;
+    cout << "unesi " << true << " - " << false << endl;
+    cin >> flag;
+    cout << noboolalpha << flag <<endl;
+    int a = 123;
+    cout << "hex " << hex << a << endl;
+    cout << "dec " << dec << a << endl;
+    cout << "oct " << oct << a << endl;
+    double pi = 3.141592;
+    cout << "pi = " << fixed;
+    cout << setprecision(7) << setw(20) << setfill('*');
+    cout << pi << endl;
 }
