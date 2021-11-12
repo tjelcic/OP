f8e449e8c986f6630e5ae179dcafc159f6761e17
Tea Jelčić <38245828+tjelcic@users.noreply.github.com>
2021-11-12 08:47:27 +0100

    Add files via upload

--- Vježba 3/Vj3Zad6/main.cpp
+++ Vježba 3/Vj3Zad6/main.cpp
@@ -9,21 +9,21 @@ vector<string> sortReverseStrings ()
 {
     vector<string> vec;
     cout << "Input the number of strings:" << endl;
-	int n;
-	cin >> n;
-	string str;
-	cout << "Input the strings:" << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> str;
-		vec.push_back(str);
-	}
+    int n;
+    cin >> n;
+    string str;
+    cout << "Input the strings:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> str;
+        vec.push_back(str);
+    }
     for (int i = 0; i < vec.size(); i++)
-	{
-		reverse(vec[i].begin(), vec[i].end());
-	}
-	sort(vec.begin(), vec.end());
-	return vec;
+    {
+        reverse(vec[i].begin(), vec[i].end());
+    }
+    sort(vec.begin(), vec.end());
+    return vec;
 }
 
 int main()
@@ -31,8 +31,8 @@ int main()
     vector<string> vec = sortReverseStrings();
     cout << "Strings reversed and sorted in ascending order:" << endl;
     for (int i = 0; i < vec.size(); i++)
-	{
-		cout << vec[i] << endl;
-	}
+    {
+        cout << vec[i] << endl;
+    }
     return 0;
 }
